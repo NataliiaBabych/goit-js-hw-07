ae0944f30c69168a5f11f711dc7945982dd1ed4e
Nataliia Babych <babicka89@gmail.com>
2021-04-11 23:03:24 +0300

    task 5 done

---
+++ src/js/task-05.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('#name-input');
+const greeting = document.querySelector('#name-output');
+
+console.log(greeting);
+
+input.addEventListener('input', onInputChange); 
+
+function onInputChange(event) {
+    console.log(event.currentTarget.value);
+    greeting.textContent = event.currentTarget.value; 
+};
+
